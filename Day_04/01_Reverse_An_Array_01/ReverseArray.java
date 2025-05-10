d402f95f2f3086d473d2b679ea21c551d6637b54
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2025-05-10 13:28:27 +0530

    Day_04

---
+++ Day_04/01_Reverse_An_Array_01/ReverseArray.java
@@ -0,0 +1,28 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+public class ReverseArray {
+    public static void reverse_array(int numbers[]){
+        int start = 0, end = numbers.length - 1;
+        
+        while(start < end) {
+            //swap the first and last
+            int temp = numbers[end];
+            numbers[end] = numbers[start];
+            numbers[start] = temp; 
+
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+
+        reverse_array(numbers);
+
+        //print array
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
